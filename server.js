9e1e9535562b6f33c4dcde36bf712f7b1a3fed67
Lucas <lucasalvarado23@gmail.com>
2018-06-03 00:46:57 -0400

    fixed server

--- server.js
+++ server.js
@@ -30,6 +30,5 @@ app.get("/", function(req, res){
 
 //var routes = require('./controllers/routes.js');
 //app.use('/',routes);
-
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
